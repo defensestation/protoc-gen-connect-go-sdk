a91fb5c185c2e35994f987de5f12fa430e7aad1d
gsingh-ds <gsingh@defensestation.ca>
2024-10-20 02:33:42 +0000

    test

--- main.go
+++ main.go
@@ -272,8 +272,7 @@ func generateServiceNameVariables(g *protogen.GeneratedFile, file *protogen.File
 	// :::Modified:::
 	g.P("init() {")
 	for _, service := range file.Services {
-		g.P(g.QualifiedGoIdent(strings.ToLower(file.GoDescriptorIdent)),
-			`_init()`)
+		g.P(g.QualifiedGoIdent(file.GoDescriptorIdent),`_init()`)
 	}
 	g.P("loadVars()")
 	g.P("}")
